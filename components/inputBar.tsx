1563147ab42f10b57d4156fe8185e500bcb69e07
Matthew Man <matthew08m@gmail.com>
2021-04-20 11:39:56 +0100

    Added input box and send button

--- components/inputBar.tsx
+++ components/inputBar.tsx
@@ -1,10 +1,38 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, View, Text, TextInput, Button } from "react-native";
 
 export default function InputBar({userId}: {userId: string}) {
-    return (
-        <View>
+    const [messageInput, setMessageInput] = useState("");
 
+    function handleSendMessage() {
+        console.log("This send button works")
+    }
+
+    return (
+        <View style={styles.container}>
+            <TextInput onChangeText={setMessageInput} value={messageInput} placeholder="Type here..." style={styles.inputBox}/>
+            <Button title="Send" onPress={handleSendMessage}/>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      display: 'flex',
+      width: '100%',
+      height: 'auto',
+      backgroundColor: '#E1E1E1',
+      alignItems: 'center',
+      alignContent: 'center',
+      justifyContent: 'flex-start',
+      flexDirection: 'row',
+    },
+    inputBox: {
+        backgroundColor: '#fff',
+        height: 35,
+        width: '80%',
+        padding: 5,
+        margin: 10,
+        borderRadius: 5,
+    }
+  });
